4915fa44246b5d756153810628732113449eaaee
Timo Siegle <timo.siegle@bechtle.com>
2016-05-16 00:46:19 +0200

    commit first release

--- velocimetro.playground/Contents.swift
+++ velocimetro.playground/Contents.swift
@@ -1,5 +1,45 @@
-//: Playground - noun: a place where people can play
+//: Un velocímetro de un automóvil digital
 
 import UIKit
 
-var str = "Hello, playground"
+enum Velocidades: Int {
+    case Apagado = 0, VelocidadBaja = 20, VelocidadMedia = 50, VelocidadAlta = 120
+    
+    init(velocidadInicial : Velocidades) {
+        self = velocidadInicial
+    }
+}
+
+class Auto {
+    var velocidad: Velocidades
+    
+    init() {
+        velocidad = Velocidades.Apagado
+    }
+    
+    func cambioDeVelocidad() ->  (actual : Int, velocidadEnCadena: String) {
+        
+        var velocidadEnCadena: String
+        switch velocidad {
+            case Velocidades.Apagado:
+                velocidad = Velocidades.VelocidadBaja
+                velocidadEnCadena = "Velocidad Baja"
+            case Velocidades.VelocidadBaja:
+                velocidad = Velocidades.VelocidadMedia
+                velocidadEnCadena = "Velocidad Media"
+            case Velocidades.VelocidadMedia:
+                velocidad = Velocidades.VelocidadAlta
+                velocidadEnCadena = "Velocidad Alta"
+            case Velocidades.VelocidadAlta:
+                velocidad = Velocidades.VelocidadMedia
+                velocidadEnCadena = "Velocidad Media"
+        }
+        return (velocidad.rawValue, velocidadEnCadena)
+    }
+}
+
+var auto = Auto()
+for index in 1...20 {
+    let result = auto.cambioDeVelocidad()
+        print("\(result.actual), \(result.velocidadEnCadena)")
+}
